4878ef51c0435c0a16e7629866bfec4c8034df5d
niknak1379 <nikanostovan@gmail.com>
2025-12-19 17:33:48 -0800

    still cicd

--- main.go
+++ main.go
@@ -6,16 +6,16 @@ import (
 	"os"
 	database "priceTracker/Database"
 	discord "priceTracker/Discord"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	// comment for docker deployment
-	// err := godotenv.Load()
-	/* if err != nil {
-		log.Fatal("Error loading .env file")
-	} */
+	godotenv.Load()
+	
 	discord.BotToken = os.Getenv("PUBLIC_KEY")
 	ctx, cancel := context.WithCancel(context.Background())
 	database.InitDB(ctx, cancel)
@@ -23,7 +23,7 @@ func main() {
 	// charts.PriceHistoryChart("5070", 3)
 	discord.Run() // call the run function of bot/bot.go
 	defer func() {
-		if err = database.Client.Disconnect(context.TODO()); err != nil {
+		if err := database.Client.Disconnect(context.TODO()); err != nil {
 			panic(err)
 		}
 		cancel()

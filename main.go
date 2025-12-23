6909b05e8204bcda1820cb803fe8cbbb5eec166e
niknak1379 <nikanostovan@gmail.com>
2025-12-22 17:25:10 -0800

    d

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"log"
 	"os"
-	crawler "priceTracker/Crawler"
 	database "priceTracker/Database"
 	discord "priceTracker/Discord"
+	scheduler "priceTracker/Scheduler"
 	"time"
 
 	"github.com/joho/godotenv"
@@ -18,14 +18,14 @@ func main() {
 	
 	godotenv.Load()
 	// crawler.GetPrice("https://www.bhphotovideo.com/c/product/1752177-REG/fractal_design_fd_c_nor1c_02_north_mid_tower_atx_case.html", "span[class^='price_']")
-	crawler.GetPrice("https://www.newegg.com/fractal-design-atx-mid-tower-meshify-3-steel-pc-case-white-fd-c-mes3a-04/p/N82E16811352227", "li.price-current strong")
+	//crawler.GetPrice("https://www.newegg.com/fractal-design-atx-mid-tower-meshify-3-steel-pc-case-white-fd-c-mes3a-04/p/N82E16811352227", "li.price-current strong")
 	discord.BotToken = os.Getenv("PUBLIC_KEY")
 	ctx, cancel := context.WithCancel(context.Background())
 	database.InitDB(ctx, cancel)
-	//go scheduler.InitScheduler(ctx, cancel)
+	go scheduler.InitScheduler(ctx, cancel)
 
 	
-	//discord.Run() 
+	discord.Run() 
 	defer func() {
 		if err := database.Client.Disconnect(context.TODO()); err != nil {
 			panic(err)

6bdcf3ce90b5535cef7772904f35d7c3242e5472
niknak1379 <nikanostovan@gmail.com>
2025-12-27 09:27:35 -0800

    added error for graphing

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ import (
 	database "priceTracker/Database"
 	discord "priceTracker/Discord"
 	"sync"
+	"time"
 
 	scheduler "priceTracker/Scheduler"
-	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -22,7 +22,7 @@ func main() {
 	var wg sync.WaitGroup
 	godotenv.Load()
 	// crawler.GetPrice("https://www.bhphotovideo.com/c/product/1752177-REG/fractal_design_fd_c_nor1c_02_north_mid_tower_atx_case.html", "span[class^='price_']")
-	//crawler.GetPrice("https://www.newegg.com/fractal-design-atx-mid-tower-meshify-3-steel-pc-case-white-fd-c-mes3a-04/p/N82E16811352227", "li.price-current strong")
+	// crawler.GetPrice("https://www.newegg.com/fractal-design-atx-mid-tower-meshify-3-steel-pc-case-white-fd-c-mes3a-04/p/N82E16811352227", "li.price-current strong")
 	discord.BotToken = os.Getenv("PUBLIC_KEY")
 	ctx, cancel := context.WithCancel(context.Background())
 	database.InitDB(ctx)
@@ -31,6 +31,7 @@ func main() {
 		discord.Run(ctx)
 	})
 
+	// make the program run unless sigINT is recieved
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt)
 	log.Println("graceful shutdown setup")

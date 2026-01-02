e711c5a52fe2bb339a1095f778a8015fa0feb59c
niknak1379 <nikanostovan@gmail.com>
2026-01-01 22:09:34 -0800

    added more robust formatting

--- Discord/formatter.go
+++ Discord/formatter.go
@@ -9,53 +9,100 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+const (
+	MaxFieldsPerEmbed = 25  // Discord limit
+	MaxFieldNameLen   = 256 // Discord limit
+	MaxFieldValueLen  = 1024 // Discord limit
+	MaxEmbedSize      = 6000 // Total characters across all fields
+)
+
 func setEmbed(Item *database.Item) []*discordgo.MessageEmbed {
 	var fields []*discordgo.MessageEmbedField
 	var retArr []*discordgo.MessageEmbed
-	// set up current price information
+
+	// Set up current price information
 	trackerFields := setTrackerFields(Item)
 	ebayFields := setSecondHandField(Item.EbayListings)
 	priceFields := setPriceField(&Item.CurrentLowestPrice, "Current")
 	lowestPriceField := setPriceField(&Item.LowestPrice, "Historically Lowest")
+	
 	fields = append(fields, trackerFields...)
 	fields = append(fields, ebayFields...)
 	fields = append(fields, priceFields...)
 	fields = append(fields, lowestPriceField...)
+
 	fmt.Println("total field length for", Item.Name, len(fields))
-	// apparently go doesnt have ceil for int division
-	// its either float conversions or this
-	for i := range (len(fields) + 20) / 21 {
-    start := i * 21
-    end := min(start+21, len(fields))
-    
-    em := discordgo.MessageEmbed{
-        Title: Item.Name,
-        Image: &discordgo.MessageEmbedImage{
-            URL:    Item.ImgURL,
-            Height: 300,
-            Width:  300,
-        },
-        Fields: fields[start:end],
-        Type:   discordgo.EmbedTypeRich,
-    }
-    retArr = append(retArr, &em)
-    fmt.Println("setting embed for indexes", start, end, len(fields))
-}
+
+	// Split fields into embeds based on Discord limits
+	currentFields := []*discordgo.MessageEmbedField{}
+	currentSize := 0
+
+	for _, field := range fields {
+		fieldSize := len(field.Name) + len(field.Value)
+		
+		// Check if adding this field would exceed limits
+		shouldCreateNewEmbed := len(currentFields) >= MaxFieldsPerEmbed || 
+			currentSize+fieldSize > MaxEmbedSize
+
+		if shouldCreateNewEmbed && len(currentFields) > 0 {
+			// Create embed with current fields
+			em := &discordgo.MessageEmbed{
+				Title: Item.Name,
+				Image: &discordgo.MessageEmbedImage{
+					URL:    Item.ImgURL,
+					Height: 300,
+					Width:  300,
+				},
+				Fields: currentFields,
+				Type:   discordgo.EmbedTypeRich,
+			}
+			retArr = append(retArr, em)
+			fmt.Printf("Created embed with %d fields, %d chars\n", len(currentFields), currentSize)
+
+			// Reset for next embed
+			currentFields = []*discordgo.MessageEmbedField{}
+			currentSize = 0
+		}
+
+		// Add field to current embed
+		currentFields = append(currentFields, field)
+		currentSize += fieldSize
+	}
+
+	// Add remaining fields as final embed
+	if len(currentFields) > 0 {
+		em := &discordgo.MessageEmbed{
+			Title: Item.Name,
+			Image: &discordgo.MessageEmbedImage{
+				URL:    Item.ImgURL,
+				Height: 300,
+				Width:  300,
+			},
+			Fields: currentFields,
+			Type:   discordgo.EmbedTypeRich,
+		}
+		retArr = append(retArr, em)
+		fmt.Printf("Created final embed with %d fields, %d chars\n", len(currentFields), currentSize)
+	}
+
 	return retArr
 }
-func setTrackerFields(Item *database.Item)[]*discordgo.MessageEmbedField{
+
+func setTrackerFields(Item *database.Item) []*discordgo.MessageEmbedField {
 	var fields []*discordgo.MessageEmbedField
-	// set up trackerArr infromation
+	
+	// Set up trackerArr information
 	field := discordgo.MessageEmbedField{
 		Name:   embedSeparatorFormatter("Tracking URL", 43),
 		Value:  embedSeparatorFormatter("CSS Selector", 44),
 		Inline: false,
 	}
 	fields = append(fields, &field)
+	
 	for _, tracker := range Item.TrackingList {
 		field := discordgo.MessageEmbedField{
-			Name:   tracker.URI,
-			Value:  tracker.HtmlQuery,
+			Name:   truncateString(tracker.URI, MaxFieldNameLen),
+			Value:  truncateString(tracker.HtmlQuery, MaxFieldValueLen),
 			Inline: false,
 		}
 		separatorField := discordgo.MessageEmbedField{
@@ -67,45 +114,51 @@ func setTrackerFields(Item *database.Item)[]*discordgo.MessageEmbedField{
 	}
 	return fields
 }
+
 func setSecondHandField(ebayArr []types.EbayListing) []*discordgo.MessageEmbedField {
 	var res []*discordgo.MessageEmbedField
 	if len(ebayArr) == 0 {
 		return res
 	}
+	
 	HeaderField := discordgo.MessageEmbedField{
 		Name: embedSeparatorFormatter("Ebay Listings", 44),
 	}
 	res = append(res, &HeaderField)
+	
 	for _, Listing := range ebayArr {
 		listFields := formatSecondHandField(Listing)
 		res = append(res, listFields...)
 	}
 	return res
 }
-func formatSecondHandField (Listing types.EbayListing) []*discordgo.MessageEmbedField{
+
+func formatSecondHandField(Listing types.EbayListing) []*discordgo.MessageEmbedField {
 	priceField := discordgo.MessageEmbedField{
-		Name:  Listing.Title,
-		Value: "$" + strconv.Itoa(Listing.Price+1),
+		Name:   truncateString(Listing.Title, MaxFieldNameLen),
+		Value:  "$" + strconv.Itoa(Listing.Price+1),
 		Inline: false,
 	}
 	conditionField := discordgo.MessageEmbedField{
-		Name:  "Condition/Location:",
-		Value: Listing.Condition,
+		Name:   "Condition/Location:",
+		Value:  truncateString(Listing.Condition, MaxFieldValueLen),
 		Inline: false,
 	}
 	urlField := discordgo.MessageEmbedField{
-		Name:  "From URL:",
-		Value: Listing.URL,
+		Name:   "From URL:",
+		Value:  truncateString(Listing.URL, MaxFieldValueLen),
 		Inline: false,
 	}
 	separatorField := discordgo.MessageEmbedField{
-		Name:  embedSeparatorFormatter("", 44),
-		Value: "",
+		Name:   embedSeparatorFormatter("", 44),
+		Value:  "",
 		Inline: false,
 	}
+	
 	var ret []*discordgo.MessageEmbedField
 	return append(ret, &priceField, &conditionField, &urlField, &separatorField)
 }
+
 func setPriceField(p *database.Price, message string) []*discordgo.MessageEmbedField {
 	priceField := discordgo.MessageEmbedField{
 		Name: embedSeparatorFormatter(fmt.Sprintf("%s Price", message), 44),
@@ -120,18 +173,30 @@ func setPriceField(p *database.Price, message string) []*discordgo.MessageEmbedF
 	}
 	urlField := discordgo.MessageEmbedField{
 		Name:   "From Price Source:",
-		Value:  p.Url,
+		Value:  truncateString(p.Url, MaxFieldValueLen),
 		Inline: false,
 	}
 	dateField := discordgo.MessageEmbedField{
-		Name: "Date:",
-		Value: p.Date.Format("2006-01-02"),
+		Name:   "Date:",
+		Value:  p.Date.Format("2006-01-02"),
 		Inline: false,
 	}
 	fmt.Println(p.Date.Format("2006-01-02"))
+	
 	var fields []*discordgo.MessageEmbedField
 	fields = append(fields, &priceField, &urlField, &dateField)
 	return fields
+}
+
+// Truncate string to max length with ellipsis
+func truncateString(s string, maxLen int) string {
+	if len(s) <= maxLen {
+		return s
+	}
+	if maxLen <= 3 {
+		return s[:maxLen]
+	}
+	return s[:maxLen-3] + "..."
 }
 
 // <-------- s --------->

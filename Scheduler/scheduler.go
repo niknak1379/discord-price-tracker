4839901918e5a69a80afa154242f6310f21a7e71
niknak1379 <nikanostovan@gmail.com>
2026-02-11 20:40:56 -0700

    s

--- Scheduler/scheduler.go
+++ Scheduler/scheduler.go
@@ -36,14 +36,14 @@ func SetChannelScheduler(ctx context.Context) {
 	itemMap := make(map[string]*database.Item)
 	// for running it once immediately on deployment
 	//
-	go func() {
-		for _, Channel := range database.ChannelMap {
-			itemsArr := database.GetAllItems(Channel.ChannelID, exludedFields)
-			for _, item := range itemsArr {
-				updateSingleItem(item, Channel)
-			}
-		}
-	}()
+	// go func() {
+	// 	for _, Channel := range database.ChannelMap {
+	// 		itemsArr := database.GetAllItems(Channel.ChannelID, exludedFields)
+	// 		for _, item := range itemsArr {
+	// 			updateSingleItem(item, Channel)
+	// 		}
+	// 	}
+	// }()
 	// Initial load for scheduler this runs after the timers hit tho not immediately
 	loadAndStartItems(ctx, activeRoutines, itemMap)
 

f03b6df1fcd0eaf095c39d4e14794f40d09eb7eb
niknak1379 <nikanostovan@gmail.com>
2026-02-15 20:59:27 -0800

    added proxy

--- Crawler/depopCrawler.go
+++ Crawler/depopCrawler.go
@@ -78,8 +78,10 @@ func CrawlDepop(Name string,
 				slog.Int("item price", price))
 			return
 		}
+		proxyCopy := make([]string, len(proxy))
+		copy(proxyCopy, proxy)
 		listing, productAttempts, err := CrawlProductPage(productURL,
-			allRegexPatterns, allSpecialWords, exclusionRegexes, exclusionSpecialWords, attempts, proxy)
+			allRegexPatterns, allSpecialWords, exclusionRegexes, exclusionSpecialWords, attempts, proxyCopy)
 		attempts = append(attempts, productAttempts...)
 		if err != nil {
 			slog.Warn("could not visit product page")
@@ -95,10 +97,14 @@ func CrawlDepop(Name string,
 	c.Wait()
 
 	if err != nil || !visited {
+		proxyVal := types.ProxyDisabled
+		if len(proxy) != 0 {
+			proxyVal = proxy[proxyIndex]
+		}
 		attempts = append(attempts, &types.Attempt{
-			Crawler:   types.CrawlerFacebook,
-			Proxy:     types.ProxyEnabled,
-			Method:    types.MethodChromeDP,
+			Crawler:   types.CrawlerDepop,
+			Proxy:     proxyVal,
+			Method:    types.MethodColly,
 			Timestamp: time.Now(),
 			Error: func(err error) string {
 				if err != nil {
@@ -108,17 +114,28 @@ func CrawlDepop(Name string,
 				}
 			}(err),
 		})
-		logger.IncidentChannel <- types.Incident{
-			StartTime: time.Now(),
-			URL:       url,
-			Domain:    "Depop",
-			Attempts:  attempts,
-			Resolved:  false,
+		if len(proxy) != 0 {
+			slog.Warn("depop proxy crawl failed, retrying without this proxy",
+				slog.Any("Error", err),
+				slog.String("proxy", proxy[proxyIndex]),
+			)
+			proxy = append(proxy[:proxyIndex], proxy[proxyIndex+1:]...)
+			return CrawlDepop(Name, Price, allRegexPatterns, allSpecialWords,
+				exclusionRegexes, exclusionSpecialWords, attempts, proxy)
+		} else {
+			slog.Error("depop crawl failed with no proxy")
+			logger.IncidentChannel <- types.Incident{
+				StartTime: time.Now(),
+				URL:       url,
+				Domain:    "Depop",
+				Attempts:  attempts,
+				Resolved:  false,
+			}
+			if err == nil {
+				err = errors.New("Depop link not visited, might have been rate limited")
+			}
+			return retArr, err
 		}
-		if err == nil {
-			err = errors.New("Depop link not visited, might have been rate limited")
-		}
-		return retArr, err
 	}
 
 	return retArr, nil
@@ -173,10 +190,39 @@ func CrawlProductPage(productURL string,
 			slog.Any("error", err),
 			slog.Bool("visited", visited),
 		)
-		if !visited {
+		if !visited && err == nil {
 			err = errors.New("page not visited")
 		}
-		return Listing, []*types.Attempt{}, err
+		proxyVal := types.ProxyDisabled
+		if len(proxy) != 0 {
+			proxyVal = proxy[proxyIndex]
+		}
+		retAttempts := []*types.Attempt{{
+			Crawler:   types.CrawlerDepop,
+			Proxy:     proxyVal,
+			Method:    types.MethodColly,
+			Timestamp: time.Now(),
+			Error: func(err error) string {
+				if err != nil {
+					return err.Error()
+				} else {
+					return errors.New("product page not visited").Error()
+				}
+			}(err),
+		}}
+		if len(proxy) != 0 {
+			slog.Warn("depop product page proxy failed, retrying without this proxy",
+				slog.String("URL", productURL),
+				slog.String("proxy", proxy[proxyIndex]),
+			)
+			proxy = append(proxy[:proxyIndex], proxy[proxyIndex+1:]...)
+			proxyCopy := make([]string, len(proxy))
+			copy(proxyCopy, proxy)
+			return CrawlProductPage(productURL,
+				allRegexPatterns, allSpecialWords, exclusionRegexes,
+				exclusionSpecialWords, attempts, proxyCopy)
+		}
+		return Listing, retAttempts, err
 	}
 	// Now approved and condition are set
 	if approved {
@@ -193,5 +239,5 @@ func CrawlProductPage(productURL string,
 			slog.String("URL", productURL),
 		)
 	}
-	return Listing, []*types.Attempt{}, err
+	return Listing, attempts, err
 }

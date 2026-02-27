27bfdba109f55aa4774d9b5194548f0f07e0a1dc
niknak1379 <nikanostovan@gmail.com>
2026-02-27 15:47:03 -0800

    have to break up scheduler functions next

--- Scheduler/itemChangeEvents.go
+++ Scheduler/itemChangeEvents.go
@@ -1,15 +1,46 @@
 package scheduler
 
-import database "priceTracker/Database"
+import (
+	"context"
 
-var itemChangeChannel chan database.Item
+	database "priceTracker/Database"
+)
+
+var itemChangeChannel chan itemChangeEvent
+
+type (
+	itemChangeEvent struct {
+		Item   *database.Item
+		Change int
+	}
+)
+
+const (
+	Edit int = iota
+	Remove
+	Add
+)
 
 func itemChangeEventBusInit() {
-	itemChangeChannel = make(chan database.Item, 100)
-	go ProcessItemChangeEvent()
+	itemChangeChannel = make(chan itemChangeEvent, 100)
+	go ProcessItemChangeEvent(context.TODO())
 }
 
-func ProcessItemChangeEvent() {
+func ProcessItemChangeEvent(ctx context.Context) {
 	for {
+		select {
+		// cases it has to handle
+		// 1. item change
+		// 2. new item and item removal
+		case Event := <-itemChangeChannel:
+			switch Event.Change {
+			case Edit:
+			case Remove:
+			case Add:
+			}
+
+		case <-ctx.Done():
+			return
+		}
 	}
 }

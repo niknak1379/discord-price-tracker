b84591afd886f7e62d9d797365a310c39cf55858
niknak1379 <nikanostovan@gmail.com>
2026-03-17 14:22:41 -0700

    bug

--- Discord/formatter.go
+++ Discord/formatter.go
@@ -112,9 +112,6 @@ func setAlternateNamesFields(Item *database.Item) []*discordgo.MessageEmbedField
 	if Item.Timer == 0 {
 		Item.Timer = 8
 	}
-	if Item.SecondHandPrice == 0 {
-		Item.SecondHandPrice = Item.CurrentLowestPrice.Price
-	}
 	timer := discordgo.MessageEmbedField{
 		Name:   embedSeparatorFormatter("Scraping Timer", 43),
 		Value:  strconv.Itoa(Item.Timer) + " h",
@@ -128,7 +125,7 @@ func setAlternateNamesFields(Item *database.Item) []*discordgo.MessageEmbedField
 	}
 	secondHandPrice := discordgo.MessageEmbedField{
 		Name:   embedSeparatorFormatter("Acceptable Second Hand Price", 43),
-		Value:  strconv.Itoa(Item.SecondHandPrice),
+		Value:  strconv.Itoa(SecondHandPrice),
 		Inline: false,
 	}
 	Fields = append(Fields, &timer, &secondHandPrice)
